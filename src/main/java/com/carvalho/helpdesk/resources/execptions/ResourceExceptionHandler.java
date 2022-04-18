bb4dcc33ea7cbb25076b12c3d6f8be7769d734ff
Alex Carvalho <leco_123carvalho@hotmailc.om>
2022-04-18 13:42:30 -0300

    HEL-6 Exceção para CPF já cadastrado

--- src/main/java/com/carvalho/helpdesk/resources/execptions/ResourceExceptionHandler.java
+++ src/main/java/com/carvalho/helpdesk/resources/execptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.carvalho.helpdesk.resources.execptions;
 
+import com.carvalho.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.carvalho.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,5 +16,11 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException ex, HttpServletRequest request){
         StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Object Not Found", ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request){
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Violação de dados", ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
--- src/main/java/com/carvalho/helpdesk/services/TecnicoService.java
+++ src/main/java/com/carvalho/helpdesk/services/TecnicoService.java
@@ -1,8 +1,11 @@
 package com.carvalho.helpdesk.services;
 
+import com.carvalho.helpdesk.domain.Pessoa;
 import com.carvalho.helpdesk.domain.Tecnico;
 import com.carvalho.helpdesk.domain.dtos.TecnicoDTO;
+import com.carvalho.helpdesk.domain.repository.PessoaRepository;
 import com.carvalho.helpdesk.domain.repository.TecnicoRepository;
+import com.carvalho.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.carvalho.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +18,8 @@ public class TecnicoService {
 
     @Autowired
     private TecnicoRepository tecnicoRepository;
+    @Autowired
+    private PessoaRepository pessoaRepository;
 
     public Tecnico findById(Integer id) {
         Optional<Tecnico> tecnico = tecnicoRepository.findById(id);
@@ -27,7 +32,21 @@ public class TecnicoService {
 
     public Tecnico create(TecnicoDTO tecnicoDTO) {
         tecnicoDTO.setId(null);
+        validaPorCPFEEmail(tecnicoDTO);
         Tecnico newTecnico = new Tecnico(tecnicoDTO);
         return tecnicoRepository.save(newTecnico);
+    }
+
+    private void validaPorCPFEEmail(TecnicoDTO tecnicoDTO) {
+        Optional<Pessoa> pessoa = pessoaRepository.findByCpf(tecnicoDTO.getCpf());
+
+        if(pessoa.isPresent() && !pessoa.get().getId().equals(tecnicoDTO.getId())){
+            throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+        }
+
+        pessoa  = pessoaRepository.findByEmail(tecnicoDTO.getEmail());
+        if(pessoa.isPresent() && !pessoa.get().getId().equals(tecnicoDTO.getId())){
+            throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
+        }
     }
 }
