9f731b020c6970ac32d6caa21fa028248fb73b56
Alex Carvalho <leco_123carvalho@hotmailc.om>
2022-04-22 20:22:32 -0300

    HEL-15 Autentication com JWT

--- src/main/java/com/carvalho/helpdesk/SecurityConfig.java
+++ src/main/java/com/carvalho/helpdesk/SecurityConfig.java
@@ -1,12 +1,17 @@
 package com.carvalho.helpdesk;
 
+import com.carvalho.helpdesk.security.JWTAutenticationFilter;
+import com.carvalho.helpdesk.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -27,6 +32,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private Environment env;
+    @Autowired
+    private JWTUtil jwtUtil;
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -38,6 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // super.configure(http);
         // aplicando regras de CORS e desabilitando o CSRF
         http.cors().and().csrf().disable();
+        http.addFilter(new JWTAutenticationFilter(authenticationManager(), jwtUtil));
         // Autorizar Requisções permita tudo que vier após esse endpoint
         // e solicite autenticação de tudo que for diferente da PUBLIC_MATCHERS
         http.authorizeRequests()
@@ -45,6 +55,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
         // garantindo que a session csrf não será criada
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
 
     /**
